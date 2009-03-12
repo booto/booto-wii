abe2653084c139ef19e91317331ec6d7953a2539
booto <remornicus@gmail.com>
2009-03-12 11:52:23 +0900

    incorporated fuller pane attributes, standard with benzin's naming

--- banner-blyt/chunk_pane.cpp
+++ banner-blyt/chunk_pane.cpp
@@ -8,25 +8,27 @@ chunk_ptr chunk_pan1::create_chunk(uint32_t id, std::streampos start, std::istre
 
 	chunk_pan1 *int_rv = new chunk_pan1(*length, start);
 
-	is.read(reinterpret_cast<char*>(&int_rv->m_unk1), sizeof(int_rv->m_unk1));
+	is.read(reinterpret_cast<char*>(&int_rv->m_flag1), sizeof(int_rv->m_flag1));
+	is.read(reinterpret_cast<char*>(&int_rv->m_flag2), sizeof(int_rv->m_flag2));
+	is.read(reinterpret_cast<char*>(&int_rv->m_alpha1), sizeof(int_rv->m_alpha1));
+	is.read(reinterpret_cast<char*>(&int_rv->m_alpha2), sizeof(int_rv->m_alpha2));
 
 	{
-		char name_tmp[0x30];
+		char name_tmp[24];
 		is.read(name_tmp, sizeof(name_tmp));
 		int_rv->m_name = name_tmp;
 	}
 
-	int_rv->m_funk1.unserialise_bin(is);
-	int_rv->m_funk2.unserialise_bin(is);
-	int_rv->m_funk3.unserialise_bin(is);
-	int_rv->m_funk4.unserialise_bin(is);
-
-/*
-	is.read(reinterpret_cast<char*>(&int_rv->m_funk1), sizeof(int_rv->m_funk1));
-	is.read(reinterpret_cast<char*>(&int_rv->m_funk2), sizeof(int_rv->m_funk2));
-	is.read(reinterpret_cast<char*>(&int_rv->m_funk3), sizeof(int_rv->m_funk3));
-	is.read(reinterpret_cast<char*>(&int_rv->m_funk4), sizeof(int_rv->m_funk4));
-*/
+	int_rv->m_x.unserialise_bin(is);
+	int_rv->m_y.unserialise_bin(is);
+	int_rv->m_z.unserialise_bin(is);
+	int_rv->m_flip_x.unserialise_bin(is);
+	int_rv->m_flip_y.unserialise_bin(is);
+	int_rv->m_angle.unserialise_bin(is);
+	int_rv->m_x_mag.unserialise_bin(is);
+	int_rv->m_y_mag.unserialise_bin(is);
+	int_rv->m_width.unserialise_bin(is);
+	int_rv->m_height.unserialise_bin(is);
 
 	is.seekg(*length + start);
 
@@ -71,20 +73,18 @@ void chunk_pan1::pretty_print(std::string prefix, std::ostream &os) const throw(
 	os << prefix << format("chunk_pan1(0x%08x)\n") % m_id;
 	os << prefix << format(" length: 0x%08x\n") % *m_length;
 	os << prefix << format(" offset: 0x%08x\n") % m_offset;
-	os << prefix << format(" unk1: 0x%08x\n") % m_unk1;
+	os << prefix << format(" flags: 0x%02x, 0x%02x\n") % (int)m_flag1 % (int)m_flag2;
+	os << prefix << format(" alpha: 0x%02x, 0x%02x\n") % (int)m_alpha1 % (int)m_alpha2;
 	os << prefix << format(" name: %s\n") % m_name.c_str();
+	os << prefix << format(" (x,y,z): (%f, %f, %f)\n") % *m_x % *m_y % *m_z;
+	os << prefix << format(" flip(x,y): (%f, %f)\n") % *m_flip_x % *m_flip_y;
 	
-	os << prefix << format(" funk1: %f\n") % *m_funk1;
-	os << prefix << format(" funk2: %f\n") % *m_funk2;
-	os << prefix << format(" funk3: %f\n") % *m_funk3;
-	os << prefix << format(" funk4: %f\n") % *m_funk4;
-	/*
-	os << prefix << format(" funk1: %f\n") % m_funk1;
-	os << prefix << format(" funk2: %f\n") % m_funk2;
-	os << prefix << format(" funk3: %f\n") % m_funk3;
-	os << prefix << format(" funk4: %f\n") % m_funk4;
-	*/
+	os << prefix << format(" angle: %f\n") % *m_angle;
+	os << prefix << format(" mag(x,y): (%f, %f)\n") % *m_x_mag % *m_y_mag;
+	os << prefix << format(" width: %f\n") % *m_width;
+	os << prefix << format(" height: %f\n") % *m_height;
 	os << prefix << format(" num_children: %d\n") % m_children.size();
+
 	for(std::vector<chunk_ptr>::const_iterator svci = m_children.begin();
 	    svci != m_children.end();
 		svci++)
--- banner-blyt/chunk_pane.hpp
+++ banner-blyt/chunk_pane.hpp
@@ -8,18 +8,22 @@
 
 class chunk_pan1: public chunk
 {
-	uint32_t m_unk1;
+
+	uint8_t m_flag1;
+	uint8_t m_flag2;
+	uint8_t m_alpha1;
+	uint8_t m_alpha2;
 	std::string m_name;
-	/*
-	float m_funk1;
-	float m_funk2;
-	float m_funk3;
-	float m_funk4;
-	*/
-	bf32 m_funk1;
-	bf32 m_funk2;
-	bf32 m_funk3;
-	bf32 m_funk4;
+	bf32 m_x;
+	bf32 m_y;
+	bf32 m_z;
+	bf32 m_flip_x;
+	bf32 m_flip_y;
+	bf32 m_angle;
+	bf32 m_x_mag;
+	bf32 m_y_mag;
+	bf32 m_width;
+	bf32 m_height;
 	
 
 	std::vector<chunk_ptr> m_children;
@@ -28,8 +32,13 @@ public:
 	static const uint32_t id = 0x316e6170L;
 	static const uint32_t pas1_id = 0x31736170L;
 	static const uint32_t pae1_id = 0x31656170L;
-	chunk_pan1(uint32_t p_length, std::streampos p_offset): chunk(id, p_length, p_offset),
-	    m_unk1(0), m_name(""), m_funk1(0.0f), m_funk2(0.0f), m_funk3(0.0f), m_funk4(0.0f)  {};
+	chunk_pan1(uint32_t p_length, std::streampos p_offset):
+		chunk(id, p_length, p_offset),
+		m_flag1(0), m_flag2(0), m_alpha1(0), m_alpha2(0), m_name(""),
+		m_x(0.0f), m_y(0.0f), m_z(0.0f),
+		m_flip_x(0.0f), m_flip_y(0.0f), m_angle(0.0f),
+		m_x_mag(0.0f), m_y_mag(0.0f),
+		m_width(0.0f), m_height(0.0f) {};
 	static chunk_ptr create_chunk(uint32_t id, std::streampos start, std::istream &is);
 	virtual void pretty_print(std::string prefix, std::ostream &os) const throw(std::ios_base::failure);
 
